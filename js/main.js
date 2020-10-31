330d97fd12be0cccd10a37970254d29be48d0bdd
yarbest <72338354+yarbest@users.noreply.github.com>
2020-10-31 13:16:54 +0200

    1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".portfolio__slider").slick({
+        infinite: false,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        // autoplay: true,
+        // autoplaySpeed: 4000,
+    });
+});
