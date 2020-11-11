45b6b2396ca6d1a3b38e6fbe15fe51df6bc29b3c
yarbest <72338354+yarbest@users.noreply.github.com>
2020-11-11 14:12:33 +0200

    1

--- js/main.js
+++ js/main.js
@@ -11,8 +11,6 @@ const closeModal = () => {
     modalWindow.classList.remove("modal_active");
 };
 
-buttonCallMe.addEventListener("click", openModal);
-buttonCloseModal.addEventListener("click", closeModal);
 //jquery
 $(document).ready(function () {
     //slider------------------------------------------------------------------
@@ -67,20 +65,10 @@ $(document).ready(function () {
     });
     //button to top-----------------------------------------------------------
 
-    //wow------------------------------------------------------------------
-    var wow = new WOW({
-        boxClass: "wow", // animated element css class (default is wow)
-        animateClass: "animated", // animation css class (default is animated)
-        offset: 0, // distance to the element when triggering the animation (default is 0)
-        mobile: true, // trigger animations on mobile devices (default is true)
-        live: true, // act on asynchronously loaded content (default is true)
-        callback: function (box) {
-            // the callback is fired every time an animation is started
-            // the argument that is passed in is the DOM node being animated
-        },
-        scrollContainer: null, // optional scroll container selector, otherwise use window,
-        resetAnimation: true, // reset animation on end (default is true)
-    });
-    wow.init();
-    //wow------------------------------------------------------------------
+    //validate phone------------------------------------------------------------------
+    $(".phone").mask("999-999-99-99", { autoclear: false });
+
+    // $(".phone").mask("9999-9999-9999-9999");
+
+    //validate phone------------------------------------------------------------------
 });
